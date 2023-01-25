46f0f9ccfcb7c35f23dac3b36891ca25781acadd
devtoju <msg.toju@outlook.de>
2023-01-25 11:44:33 +0100

    Init chaining exceptions

--- src/com/devToJu/ExceptionHandling/Main.java
+++ src/com/devToJu/ExceptionHandling/Main.java
@@ -2,11 +2,6 @@ package com.devToJu.ExceptionHandling;
 
 public class Main {
     public static void main(String[] args) {
-        /** ATTENTION:
-         * if Doing- and ClosingException occurs,
-         * only the first exception get caught!
-         * */
-
         try (var criticalResource = new CriticalResource()) {
             criticalResource.doStuff();
         }
