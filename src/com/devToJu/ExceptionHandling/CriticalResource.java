8065e55c4a593ab73f23411b7e158733aef5939c
devtoju <msg.toju@outlook.de>
2023-01-25 11:56:42 +0100

    use the cause param in the exception constructor

--- src/com/devToJu/ExceptionHandling/CriticalResource.java
+++ src/com/devToJu/ExceptionHandling/CriticalResource.java
@@ -2,38 +2,22 @@ package com.devToJu.ExceptionHandling;
 
 public class CriticalResource implements AutoCloseable {
     public CriticalResource() throws CriticalResourceException {
-        if (isRandomTrue(0.7)) {
-            var criticalRessourceException = new CriticalResourceException();
-            var openingException = new OpeningException();
-
-            criticalRessourceException.initCause(openingException);
-            throw criticalRessourceException;
-        }
-
+        if (isRandomTrue(0.7))
+            throw new CriticalResourceException(new OpeningException());
 
         System.out.println("Critical resource reserved!");
     }
 
     public void doStuff() throws CriticalResourceException {
-        if (isRandomTrue(0.5)) {
-            var criticalRessourceException = new CriticalResourceException();
-            var doingException = new DoingException();
-
-            criticalRessourceException.initCause(doingException);
-            throw criticalRessourceException;
-        }
+        if (isRandomTrue(0.5))
+            throw new CriticalResourceException(new DoingException());
 
         System.out.println("Processing...");
     }
 
     public void close() throws CriticalResourceException {
-        if (isRandomTrue(0.4)) {
-            var criticalRessourceException = new CriticalResourceException();
-            var closingException = new ClosingException();
-
-            criticalRessourceException.initCause(closingException);
-            throw criticalRessourceException;
-        }
+        if (isRandomTrue(0.4))
+            throw new CriticalResourceException(new ClosingException());
 
         System.out.println("Critical resource released!");
     }
--- src/com/devToJu/ExceptionHandling/CriticalResourceException.java
+++ src/com/devToJu/ExceptionHandling/CriticalResourceException.java
@@ -1,4 +1,7 @@
 package com.devToJu.ExceptionHandling;
 
-public class CriticalResourceException extends Exception{
+public class CriticalResourceException extends Exception {
+    public CriticalResourceException(Throwable cause) {
+        super(cause);
+    }
 }
